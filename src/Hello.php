2e1a890e33c5859ff72c06213ff0fc540deaaa68
leorain <woshilihexin@gmail.com>
2022-04-28 17:37:44 +0800

    init

---
+++ src/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Leorain\SelfComposer;
+
+class Hello
+{
+    public static function say()
+    {
+        echo "hello";
+    }
+}
---
+++ test/demo.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../vendor/autoload.php";
+
+use Leorain\SelfComposer\Hello;
+
+Hello::say();
